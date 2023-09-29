d2b229d593b6deb6abe8a53979a599730bb7368b
Jonathaannn <jonathanjog237@gmail.com>
2023-09-29 20:03:25 -0300

    Creating indicator of total activities and activities done

--- src/components/BoxTask/index.jsx
+++ src/components/BoxTask/index.jsx
@@ -1,46 +1,70 @@
-import Task from "../Task"
+import Task from "../Task";
 import InputText from "../InputText";
-import Styles from "./style.module.css"
+import Styles from "./style.module.css";
 import { useEffect, useState } from "react";
 
 function BoxTask() {
-    const [list, setList] = useState([])
-    const [task, setTask] = useState("")
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
 
-    useEffect(() => {
-        if (task !== "") {
-            setList([...list, task])
-            setTask("")
-        }
-    }, [task, list])
-
-    const addTask = (item) => {
-        const newItem = {
-            id: Math.random(),
-            task: item
-        }
-        setTask(newItem)
+  useEffect(() => {
+    if (task !== "") {
+      setList([...list, task]);
+      setTask("");
     }
+  }, [task, list]);
 
-    const deleteTask = (id) => {
-        const newList = list.filter((item) => item.id !== id)
-        setList(newList)
-    }
+  const addTask = (item) => {
+    const newItem = {
+      id: Math.random(),
+      task: item,
+      status: false,
+    };
+    setTask(newItem);
+  };
 
-    return (
-        <>
-        <InputText handleInput={addTask}/>
-        <div className={`${Styles.indicator} ${Styles.container}`}>
-            <p>Tecnologias criadas <span>0</span></p>
-            <p style={{color:"#8284FA"}}>Concluídas <span>0</span></p>
-        </div>
-        {/* <div className={`${Styles.boxText} ${Styles.container}`}>
+  const deleteTask = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
+
+  const changeStatus = (id) => {
+    const updtadeItem = list.map((item) => {
+      if (item.id === id) {
+        return { ...item, status: !item.status };
+      }
+      return item;
+    });
+    setList(updtadeItem);
+  };
+
+  return (
+    <>
+      <InputText handleInput={addTask} />
+      <div className={`${Styles.indicator} ${Styles.container}`}>
+        <p>
+          Tecnologias criadas <span>{list.length}</span>
+        </p>
+        <p style={{ color: "#8284FA" }}>
+          Concluídas{" "}
+          <span>
+            {list.filter((item) => item.status !== false).length} de{" "}
+            {list.length}
+          </span>
+        </p>
+      </div>
+        {list.length === 0 && <div className={`${Styles.boxText} ${Styles.container}`}>
             <h3>Você ainda não tem tecnologias cadastradas</h3>
             <p>Crie tecnologia e organize seus itens a fazer</p>
-        </div> */}
-        <Task customClass={Styles.container} listTasks={list} handleDelete={deleteTask}/>
-        </>
-    )
+        </div>}
+      <Task
+        customClass={Styles.container}
+        listTasks={list}
+        handleDelete={deleteTask}
+        handleStatus={changeStatus}
+      />
+    </>
+  );
 }
 
-export default BoxTask
+export default BoxTask;
--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,15 +1,19 @@
 import Styles from "./style.module.css";
 import { AiOutlineDelete } from "react-icons/ai";
 
-function Task({ customClass, listTasks, handleDelete }) {
+function Task({ customClass, listTasks, handleDelete, handleStatus }) {
   return (
     <>
       {listTasks.map((Task) => (
-        <div className={`${Styles.task} ${customClass}`}>
-          <input type="checkbox" />
+        <div className={`${Styles.task} ${customClass}`} key={Task.id}>
+          <input
+            type="checkbox"
+            checked={Task.status}
+            onChange={() => handleStatus(Task.id)}
+          />
           <label className={Styles.check}></label>
           <p>{Task.task}</p>
-          <button key={Task.id} onClick={() => handleDelete(Task.id)}>
+          <button onClick={() => handleDelete(Task.id)}>
             <AiOutlineDelete />
           </button>
         </div>
